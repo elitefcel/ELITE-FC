fecf179251e1893ce2f0a7e4a8f236c198b572e8
Wilden Farias <wildenfarias@gmail.com>
2026-08-07 13:40:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,9 @@ const titulo = document.querySelector(".hero h1");
 
 document.addEventListener("mousemove", (evento) => {
 
-    const x = (evento.clientX / window.innerWidth - 0.5) * 20;
-    const y = (evento.clientY / window.innerHeight - 0.5) * 20;
+    const x = (evento.clientX / window.innerWidth - 0.5) * 15;
+    const y = (evento.clientY / window.innerHeight - 0.5) * 15;
 
     titulo.style.transform = `translate(${x}px, ${y}px)`;
-});
-
-const links = document.querySelectorAll("nav a");
-
-links.forEach((link) => {
-
-    link.addEventListener("mouseenter", () => {
-        link.style.transform = "scale(1.1)";
-        link.style.transition = "0.3s";
-    });
-
-    link.addEventListener("mouseleave", () => {
-        link.style.transform = "scale(1)";
-    });
 
 });
